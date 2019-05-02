2df851f104cd2ffa70d9dd3594ece0f785b977a1
roshra <roshmohanr@gmail.com>
2019-05-02 08:47:14 +0530

    datatype conversion

---
+++ datatype_conversion.py
@@ -0,0 +1,12 @@
+#convert data type float to int using int(variable)
+x = 15.66
+y = int(x)
+print("float is now converted to int value : " ,y)
+
+a = 5
+b = float(5)
+print("int is now converted to float value : ", b)
+
+c = 10
+d = complex(c)
+print("int is now converted to complex value: ", d)
