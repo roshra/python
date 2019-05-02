ebcf9b977df8ef048d83edfbd1be6e36040eccac
roshra <roshmohanr@gmail.com>
2019-05-02 10:37:46 +0530

    data type conversion

---
+++ octal_binary_hex_conversion.py
@@ -0,0 +1,12 @@
+# Python program converts octal, binary and hex to decimal number system
+
+s1 = "17"
+s2 = "11100101"
+s3 = "1c2"
+
+n1 = int(s1,8)
+n2 = int(s2,2)
+n3 = int(s1,16)
+print ("octal 17 converted to decimal value is : ",n1)
+print ("binary 2 converted to decimal value is : ",n2)
+print ("Hex 16 converted to decimal value is : ",n3)
