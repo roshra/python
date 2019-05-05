1ccdab393d492f1d08063aac73dd2eed7d82d09b
roshra <roshmohanr@gmail.com>
2019-05-05 18:00:15 +0530

    data type

---
+++ string_datatype.py
@@ -0,0 +1,16 @@
+str1 = "Welcome1"
+str2 = "weclome2"
+str3 = """this is tripple quotes str3"""
+str4 = '''this is tripple quotes str4'''
+print("printing str1 ", str1)
+print("printing str2 ", str2)
+# Print single index
+print(str1[0])
+# Print index from 0 to 5
+print(str1[0:6])
+# Print index from end
+print ("printing str1 from end ", str1[-4])
+# Print * 2
+print("printing str1 two times " , str1*2)
+# Print str3
+print("printing str3 ", str4)
