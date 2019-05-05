252a541d3abf1092311b31b29c7b590f004439fe
roshra <roshmohanr@gmail.com>
2019-05-05 18:39:16 +0530

    data type

--- list_datatype.py
+++ list_datatype.py
@@ -4,3 +4,5 @@ print("printing 0th element in list ", list[0])
 print("printing 0th to 2nd elements in list ", list[0:3])
 print("printing last element of the list", list[-1])
 print("duplicating the list ", list * 2)
+list[0] = 80
+print("modified list ", list[0])
