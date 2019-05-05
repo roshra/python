576b34b6f46a34d03c57db88f0a42d2f58fff43f
roshra <roshmohanr@gmail.com>
2019-05-06 01:10:43 +0530

    data type

---
+++ sets_datatype.py
@@ -0,0 +1,16 @@
+s = {10,20,30,40}
+print(s)
+
+
+# set converts a lst list to set by using keyword set 
+lst = [1,2,3,4,5]
+s = set(lst)
+print(s)
+
+# add elelemts 50 and 60 to set
+s.update([50,60])
+print(s)
+
+# remove element from set
+s.remove(50)
+print(s)
