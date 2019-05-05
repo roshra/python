9859c6d300eeb98eab202204885074c43ca10d39
roshra <roshmohanr@gmail.com>
2019-05-05 18:29:27 +0530

    data type

---
+++ list_datatype.py
@@ -0,0 +1,6 @@
+list = [10,20,15.5,"rosh", 'mary']
+print("printing all the elements in list ",list)
+print("printing 0th element in list ", list[0])
+print("printing 0th to 2nd elements in list ", list[0:3])
+print("printing last element of the list", list[-1])
+print("duplicating the list ", list * 2)
