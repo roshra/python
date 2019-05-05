b430cace37a3502902ff34e16249da68bbef97b2
roshra <roshmohanr@gmail.com>
2019-05-05 19:01:05 +0530

    data type

---
+++ tuple_datatype.py
@@ -0,0 +1,7 @@
+# tuple is a mix of different data types enclosed in parentheses seperated by comans
+tuple = (10,-20,15.5, 'rosh', 'mary')
+print("first tuple element ", tuple[0])
+
+# tuple can't be modified and will give an error
+tuple[0] = 80
+
